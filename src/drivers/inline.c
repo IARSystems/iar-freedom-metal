91464cac0e01f246d9eb11ff84a78293b43cbf22
Lars-Ove Karlsson <lars-ove.karlsson@iar.com>
2020-02-10 13:13:43 +0100

    Added a dummy (discard-able) section to entry.S to make sure inline.c is linked to.

--- src/drivers/inline.c
+++ src/drivers/inline.c
@@ -1,4 +1,10 @@
 /* Copyright 2019 SiFive, Inc */
 /* SPDX-License-Identifier: Apache-2.0 */
 
-#include <metal/machine/inline.h>
+#ifdef __IAR_SYSTEMS_ICC__
+#define __NO_IAR_WEAK__
+const char __iar_metal_dummy=0;
+
+#endif
+
+#include "metal/machine/inline.h"
