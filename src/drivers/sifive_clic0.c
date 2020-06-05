3c8685ee7368d8d07af3391afb1c7841fafc775b
Lars-Ove Karlsson <lars-ove.karlsson@iar.com>
2020-06-05 14:38:27 +0200

    Prel attempt to convert gcc stuff with alignment

--- src/drivers/sifive_clic0.c
+++ src/drivers/sifive_clic0.c
@@ -28,13 +28,16 @@ const struct __metal_clic_cfg __metal_clic_defaultcfg = {
     .nvbit = METAL_CLIC_NONVECTOR};
 
 void __metal_clic0_handler(int id, void *priv) __attribute__((aligned(64)));
-
-void __metal_clic0_default_vector_handler(void)
+#ifndef __IAR_SYSTEMS_ICC__
+void __metal_clic0_default_vector_handler (void)
     __attribute__((interrupt, aligned(64)));
-
+#else
+__interrupt void __metal_clic0_default_vector_handler (void);
+#endif
 struct __metal_clic_cfg
 __metal_clic0_configuration(struct __metal_driver_sifive_clic0 *clic,
-                            struct __metal_clic_cfg *cfg) {
+                            struct __metal_clic_cfg *cfg)
+{
     volatile unsigned char val;
     struct __metal_clic_cfg cliccfg;
     unsigned long control_base = __metal_driver_sifive_clic0_control_base(
@@ -584,7 +587,7 @@ int __metal_driver_sifive_clic0_vector_register(
             clic->metal_mtvt_table[id] = isr;
             clic->metal_exint_table[id].exint_data = priv;
         } else {
-            clic->metal_mtvt_table[id] = __metal_clic0_default_vector_handler;
+            clic->metal_mtvt_table[id] = (metal_interrupt_vector_handler_t) __metal_clic0_default_vector_handler;
             clic->metal_exint_table[id].sub_int = priv;
         }
         rc = 0;
